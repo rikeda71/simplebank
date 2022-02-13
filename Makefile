5f56d24cbfccf78b6ab144b3330fd6c814d18e5e
s14t284 <rikeda71@gmail.com>
2022-02-13 12:44:00 +0900

    :sparkles: init api server with gin

--- Makefile
+++ Makefile
@@ -10,6 +10,8 @@ migrateup:
 # 	migrate -path db/migration -database "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable" -verbose down
 test:
 	go test -v -cover ./...
+server:
+	go run main.go
 
 ent:
 	go generate ./ent
---
+++ api/account.go
@@ -0,0 +1,81 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/s14t284/simplebank/ent"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.DbClient.Account.Create().
+		SetOwner(req.Owner).
+		SetCurrency(req.Currency).
+		SetBalance(0).
+		Save(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID int `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	account, err := server.store.DbClient.Account.Get(ctx, req.ID)
+	if err != nil {
+		// FIXME: これはうまくいかないはずなので、実際にはこのようなハンドリングはしない
+		ne := ent.NotFoundError{}
+		if err == &ne {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type listAccountRequest struct {
+	PageID   int `form:"page_id" binding:"required,min=1"`
+	PageSize int `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req listAccountRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	accounts, err := server.store.DbClient.Account.Query().
+		Limit(req.PageSize).
+		Offset(req.PageID).
+		All(ctx)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+}
